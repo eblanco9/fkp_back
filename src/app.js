6eb75679ebcc195b2d0cfbbec901ddca08d2b952
Angel Rojas <angelrojas5306@gmail.com>
2025-11-03 20:32:37 -0300

    fix cors

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ import usuarioRouter from './modules/usuarios/usuario.router.js';
 import historialRouter from './modules/historial/historial.router.js';
 import authRouter from './modules/auth/auth.router.js'
 import userAdminRouter from './modules/user-admin/user-admin.router.js'
+import cors from 'cors';
 
 const app = express();
+
+// CORS (poné tu frontend en origin si querés restringir)
+app.use(
+  cors({
+    origin: '*',          // o 'http://localhost:5173' por ejemplo
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 // Middleware global
 app.use(express.json());
